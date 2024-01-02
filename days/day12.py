03e6774be89b323a1d1c6c4f313b69a08cd56f00
Ryder Lewis <ryder@stripe.com>
2024-01-02 12:56:56 -0500

    day 12 part 2

--- days/day12.py
+++ days/day12.py
@@ -1,5 +1,6 @@
 from .day import Day
 from collections import namedtuple
+from functools import cache
 
 Record = namedtuple('Record', ['sections', 'counts'])
 
@@ -9,45 +10,38 @@ class Day12(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        records = self.parse_records()
-        s = 0
-        for rec in records:
-            ways = self.count_ways(rec, 0)
-            print(f"{rec}: {ways}")
-            s += ways
-        return str(s)
+        records = self.parse_records(1)
+        return str(sum(self.count_ways(rec) for rec in records))
 
     def part2(self) -> str:
-        return "dayXX 2"
+        records = self.parse_records(5)
+        return str(sum(self.count_ways(rec) for rec in records))
 
-    def parse_records(self) -> list[Record]:
+    def parse_records(self, copies: int) -> list[Record]:
         ret = []
         for line in self.data_lines():
             record, counts = line.split()
+            record = '?'.join([record for _ in range(copies)])
+            counts = ','.join([counts for _ in range(copies)])
             sections = tuple(x for x in record.split('.') if len(x) > 0)
             ret.append(Record(sections, tuple(map(int, counts.split(',')))))
         return ret
 
     @staticmethod
-    def count_ways(rec: Record, depth: int) -> int:
-        spaces = ' ' * depth * 2
-        print(f"{spaces}CALLED: {rec}")
+    @cache
+    def count_ways(rec: Record) -> int:
         if len(rec.counts) == 0:
             if all(s.strip('?') == '' for s in rec.sections):
-                print(f"{spaces}HERE A1 {rec}")
                 return 1
             else:
-                print(f"{spaces}HERE A0 {rec}")
                 return 0
         sections = rec.sections
         while len(sections) > 0 and len(sections[0]) < rec.counts[0]:
             if all(s == '?' for s in sections[0]):
                 sections = sections[1:]
             else:
-                print(f"{spaces}HERE B {rec}, {sections}")
                 return 0
         if len(sections) == 0:
-            print(f"{spaces}HERE C {rec}")
             return 0
 
         # for this first section, find all the possible ways that the exact count
@@ -55,27 +49,20 @@ class Day12(Day):
         ways = 0
         # allow for the possibility that the first section does not have any springs at all
         if all(s == '?' for s in sections[0]):
-            print(f"{spaces}HERE D {rec}")
-            ways += Day12.count_ways(Record(sections=sections[1:], counts=rec.counts), depth+1)
+            ways += Day12.count_ways(Record(sections=sections[1:], counts=rec.counts))
         for i in range(len(sections[0])):
             test = sections[0][i:]
-            print(f"{spaces}HERE E {rec}: {test}")
             if len(test) < rec.counts[0]:
-                print(f"{spaces}HERE E2 {rec}: {test}")
                 break
             elif len(test) == rec.counts[0]:
-                print(f"{spaces}HERE E3 {rec}: {test}")
-                ways += Day12.count_ways(Record(sections=sections[1:], counts=rec.counts[1:]), depth+1)
+                ways += Day12.count_ways(Record(sections=sections[1:], counts=rec.counts[1:]))
             else:
                 if test[rec.counts[0]] != '#':
                     section2 = test[rec.counts[0]+1:]
-                    print(f"{spaces}HERE F {rec}: {test}, {section2}")
                     if len(section2) > 0:
-                        ways += Day12.count_ways(Record(sections=(section2,)+sections[1:], counts=rec.counts[1:]), depth+1)
+                        ways += Day12.count_ways(Record(sections=(section2,)+sections[1:], counts=rec.counts[1:]))
                     else:
-                        ways += Day12.count_ways(Record(sections=sections[1:], counts=rec.counts[1:]), depth+1)
-                print(f"{spaces}HERE G {rec}: {ways}")
+                        ways += Day12.count_ways(Record(sections=sections[1:], counts=rec.counts[1:]))
             if test[0] == '#':
-                print(f"{spaces}HERE H {rec}: {test}")
                 break
         return ways
