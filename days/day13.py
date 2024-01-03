d452a62f7c7a31e24cd3e74cb9ecd6d03bb495cd
Ryder Lewis <ryder@stripe.com>
2024-01-02 19:49:21 -0500

    day 13 part 2

--- days/day13.py
+++ days/day13.py
@@ -4,24 +4,24 @@ class Grid:
     def __init__(self, rows):
         self.rows = rows
 
-    def vertical_mirrors(self):
+    def vertical_mirrors(self, smudge_count: int):
         for i in range(1, len(self.rows[0])):
-            all_good = True
+            smudges = 0
             for left, right in zip(range(i-1, -1, -1), range(i, len(self.rows[0]))):
-                if any(self.rows[r][left] != self.rows[r][right] for r in range(len(self.rows))):
-                    all_good = False
+                smudges += len([1 for r in range(len(self.rows)) if self.rows[r][left] != self.rows[r][right]])
+                if smudges > smudge_count:
                     break
-            if all_good:
+            if smudges == smudge_count:
                 yield i
 
-    def horizontal_mirrors(self):
+    def horizontal_mirrors(self, smudge_count: int):
         for i in range(1, len(self.rows)):
-            all_good = True
+            smudges = 0
             for top, bottom in zip(range(i-1, -1, -1), range(i, len(self.rows))):
-                if self.rows[top] != self.rows[bottom]:
-                    all_good = False
+                smudges += len([1 for c in range(len(self.rows[top])) if self.rows[top][c] != self.rows[bottom][c]])
+                if smudges > smudge_count:
                     break
-            if all_good:
+            if smudges == smudge_count:
                 yield i
 
     def __str__(self) -> str:
@@ -40,14 +40,20 @@ class Day13(Day):
     def part1(self) -> str:
         s = 0
         for grid in self.parse():
-            v = sum(grid.vertical_mirrors())
-            h = sum(grid.horizontal_mirrors())
+            v = sum(grid.vertical_mirrors(0))
+            h = sum(grid.horizontal_mirrors(0))
             # print(f"{grid}: {v} + 100*{h}")
             s += v + 100*h
         return str(s)
 
     def part2(self) -> str:
-        return "dayXX 2"
+        s = 0
+        for grid in self.parse():
+            v = sum(grid.vertical_mirrors(1))
+            h = sum(grid.horizontal_mirrors(1))
+            # print(f"{grid}: {v} + 100*{h}")
+            s += v + 100*h
+        return str(s)
 
     def parse(self) -> tuple[Grid, ...]:
         grids = []
